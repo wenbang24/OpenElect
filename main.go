f284b3f39240a5c2eba741923abce2b4d95c5892
wenbang24 <wenbangalt@gmail.com>
2024-11-07 20:44:49 +1100

    frontend 😔

--- main.go
+++ main.go
@@ -3,19 +3,29 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 func main() {
 	r := gin.Default()
+	r.StaticFile("/favicon.ico", "./static/favicon.ico")
+	r.StaticFile("/style.css", "./css/style.css")
+	r.StaticFile("/icon.png", "./static/icon.png")
 	r.LoadHTMLGlob("templates/*")
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"text": "Hello, World!!!!!!!!!!",
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"text": time.Now(),
 		})
 	})
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+		})
+	})
+	r.GET("/:candidate", func(c *gin.Context) {
+		name := c.Param("candidate")
+		c.HTML(http.StatusOK, "candidate.tmpl", gin.H{
+			"name": name,
 		})
 	})
 	r.Run()
